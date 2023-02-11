5e7e657ab7d97ab25af5282a966ae5797daaa222
anyaloan <117948352+anyaloan@users.noreply.github.com>
2023-02-11 23:25:29 +0400

    Update urls.py

--- actor/urls.py
+++ actor/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('actors1111111/', views.index, name='index1'),
+    path('actors/', views.index, name='index1'),
     path("actor/detail/<int:pk>/", views.actor, name="detail"),
     path("search_actor/", views.SearchActor.as_view(), name='search_actor'),
     path("actors/add_actor/", views.add_actor, name='add_actor')
